220e3c733df8cc6edc525092c1c2a9812ada6f39
Rangeet Pan <rangeet@iastate.edu>
2019-12-09 09:03:02 -0600

    initial code for imperative NN

--- Code/integration.py
+++ Code/integration.py
@@ -17,16 +17,4 @@ for i in range(100):
     NN.backwardPropagation(x_train,y_train,loss,node_hidden, node_output)
     accuracy=NN.accuracyComputation(x_train,y_train)
     print("Iteration: "+str(i)+" Accuracy: "+str(accuracy))
-# =============================================================================
-# for ii in range(200):
-#     loss = network.train(mnist.train.images, mnist.train.labels)
-#     train_accuracy=network.run(mnist.train.images,mnist.train.labels)
-#     test_accuracy=network.run(mnist.test.images,mnist.test.labels)
-#     validation_accuracy=network.run(mnist.validation.images,mnist.validation.labels)
-#     losses['train'].append(loss)
-#     losses['train_accuracy'].append(train_accuracy)
-#     losses['test_accuracy'].append(test_accuracy)
-#     losses['validation_accuracy'].append(validation_accuracy)
-#     if ii % 10 ==0:
-#         print('interation %d --loss %f -- train accuracy:%.2f ----test accuracy: %.2f ---- validation accuracy: %.2f' % (ii,loss,train_accuracy,test_accuracy,validation_accuracy))
-# =============================================================================
+
