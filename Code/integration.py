4eaf3dfc19df4a467c9b25c8c1781360541867d9
Rangeet Pan <rangeet@iastate.edu>
2019-12-09 13:25:51 -0600

    add working training code

--- Code/integration.py
+++ Code/integration.py
@@ -6,16 +6,17 @@ Created on Fri Dec  6 20:24:46 2019
 @author: rangeetpan
 """
 from util import Util
-from tensorflow.examples.tutorials.mnist import input_data
-input_data = input_data.read_data_sets('MNIST_data')
-x_train=input_data.train.images
-y_train=input_data.train.labels
+#from tensorflow.examples.tutorials.mnist import input_data
+from keras.datasets import mnist
+(X_train, Y_train), (x_test, y_test) = mnist.load_data()
+X_train=X_train.astype('float32')/255.0
+X_train=X_train.reshape(60000,784)
 NN = Util(784, 100, 10, 0.2)
 for i in range(100):
-    loss, node_hidden, node_output=NN.forwardPropagation(x_train, y_train)
+    loss, node_hidden, node_output=NN.forwardPropagation(X_train, Y_train)
     #print(loss)
-    NN.backwardPropagation(x_train,y_train,loss,node_hidden, node_output)
-    accuracy=NN.accuracyComputation(x_train,y_train)
+    NN.backwardPropagation(X_train,Y_train,loss,node_hidden, node_output)
+    accuracy=NN.accuracyComputation(X_train,Y_train)
     print("Iteration: "+str(i)+" Accuracy: "+str(accuracy))
 # =============================================================================
 # for ii in range(200):
