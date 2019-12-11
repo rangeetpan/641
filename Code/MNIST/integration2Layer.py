fa177f5d160e860dd776c4d8e112ca9d8873188a
Rangeet Pan <rangeet@iastate.edu>
2019-12-11 10:15:55 -0600

    parameterized

---
+++ Code/MNIST/integration2Layer.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 11 02:46:19 2019
+
+@author: rangeetpan
+"""
+
+
+from __future__ import print_function
+
+# Import MNIST data
+# =============================================================================
+# from tensorflow.examples.tutorials.mnist import input_data
+# mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+# =============================================================================
+from keras.datasets import mnist
+from keras.utils import to_categorical
+import numpy as np
+from util2Layer import Util
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train=x_train.astype('float32')/255.0
+x_train=x_train.reshape(60000,784)
+y_train=to_categorical(y_train)
+y_train=y_train.astype('float64')
+for i in range(0,50):
+    # Parameters
+    n_hidden_1 = 256 # 1st layer number of neurons
+    n_hidden_2 = 256
+    n_input = 784
+    count=0
+    learning_rate = 0.001
+    training_epochs = 10
+    batch_size = 100
+    delta=0.002+i*0.0005
+    delta1=[ [ +delta ] * n_hidden_1 ] * n_input
+    delta2=[ [ +delta ] * n_hidden_2 ] * n_hidden_1 # change delta
+    delta3=[ +delta ] * n_hidden_1
+    delta4= [ +delta ] * n_hidden_2   # change delta
+    exit_flag=False
+    n_classes = 10 # MNIST total classes (0-9 digits)
+    NN=Util(n_input,n_classes,n_hidden_1,n_hidden_2)
+    loss, accuracy=NN.train(x_train,y_train,learning_rate,training_epochs,batch_size)
+    text_file = open("Output_mnist_Layer2_"+str(delta)+".txt", "w")
+    text_file.write("Actual Accuracy: "+ str(accuracy)+"\n")
+    while exit_flag ==False:
+        NN.weights['h1'].assign_add(delta1)
+        NN.weights['h2'].assign_add(delta2)
+        NN.biases['b1'].assign_add(delta3)
+        NN.biases['b2'].assign_add(delta4)
+        loss1, accuracy=NN.train(x_train,y_train,learning_rate,training_epochs,batch_size)
+        text_file.write("Trial "+str(count)+" ADNN Accuracy: "+str(accuracy)+"\n")
+        count=count+1
+        obj=loss-loss1
+        loss=loss1
+        if(obj<=0):
+            exit_flag ==False
+        else:
+            P = np.exp((-obj/loss1))
+            P_rand = np.random.uniform(0,1)
+            if P_rand <= P:
+                exit_flag=False
+            else:
+                exit_flag=True
---
+++ Code/MNIST/util4Layer.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 11 01:52:16 2019
+
+@author: rangeetpan
+"""
+
+
+import tensorflow as tf
+class Util:
+    def __init__(self, n_input, n_classes, n_hidden_1, n_hidden_2,n_hidden_3,n_hidden_4):
+# tf Graph input
+        self.X = tf.placeholder("float", [None, n_input])
+        self.Y = tf.placeholder("float", [None, n_classes])
+        
+        # Store layers weight & bias
+        self.weights = {
+            'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+            'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+            'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
+            'h4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4])),
+            'out': tf.Variable(tf.random_normal([n_hidden_4, n_classes]))
+        }
+        self.biases = {
+            'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+            'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+            'b3': tf.Variable(tf.random_normal([n_hidden_3])),
+            'b4': tf.Variable(tf.random_normal([n_hidden_4])),
+            'out': tf.Variable(tf.random_normal([n_classes]))
+        }
+
+
+# Create model
+    def multilayer_perceptron(self,x):
+        # Hidden fully connected layer with 256 neurons
+        #weights['h1'].assign_add(d)
+        layer_1 = tf.add(tf.matmul(x, self.weights['h1']), self.biases['b1'])
+        # Hidden fully connected layer with 256 neurons
+        layer_2 = tf.add(tf.matmul(layer_1, self.weights['h2']), self.biases['b2'])
+        layer_3 = tf.add(tf.matmul(layer_2, self.weights['h3']), self.biases['b3'])
+        layer_4 = tf.add(tf.matmul(layer_3, self.weights['h4']), self.biases['b4'])
+        # Output fully connected layer with a neuron for each class
+        out_layer = tf.matmul(layer_4, self.weights['out']) + self.biases['out']
+        return out_layer
+    
+    # Construct model
+    def train(self,x_train,y_train,learning_rate,training_epochs,batch_size):
+        logits = self.multilayer_perceptron(self.X)
+        
+        # Define loss and optimizer
+        loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            logits=logits, labels=self.Y))
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        train_op = optimizer.minimize(loss_op)
+        # Initializing the variables
+        init = tf.global_variables_initializer()
+        
+        with tf.Session() as sess:
+            sess.run(init)
+        
+            # Training cycle
+            for epoch in range(training_epochs):
+                avg_cost = 0.
+                total_batch = int(600)
+                # Loop over all batches
+                for i in range(total_batch):
+                    batch_x=x_train[100*i:100*(i+1)]
+                    batch_y=y_train[100*i:100*(i+1)]
+                    #batch_x, batch_y = mnist.train.next_batch(batch_size)
+                    # Run optimization op (backprop) and cost op (to get loss value)
+                    _, c = sess.run([train_op, loss_op], feed_dict={self.X: batch_x,
+                                                                    self.Y: batch_y})
+                    # Compute average loss
+                    avg_cost += c / total_batch
+        
+            # Test model
+            pred = tf.nn.softmax(logits)  # Apply softmax to logits
+            correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(self.Y, 1))
+            # Calculate accuracy
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+            return avg_cost,accuracy.eval({self.X: x_train, self.Y: y_train})
